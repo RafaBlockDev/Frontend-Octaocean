b7965c41d3387984353d6603126c069d0287bd23
RafaBlockDev <rafafuentesrangel@outlook.com>
2023-07-06 21:28:06 -0600

    fix: delete /

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     //baseUrl: 'http://localhost:4500',
-    baseUrl: "https://api-octaocean.onrender.com/",
+    baseUrl: "https://api-octaocean.onrender.com",
 };
