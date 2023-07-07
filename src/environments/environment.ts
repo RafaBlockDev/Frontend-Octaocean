16192cb60603f774f460c4124ee93b9ecf80b059
RafaBlockDev <rafafuentesrangel@outlook.com>
2023-07-06 21:26:25 -0600

    test: api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    baseUrl: 'http://localhost:4500',
+    //baseUrl: 'http://localhost:4500',
+    baseUrl: "https://api-octaocean.onrender.com/",
 };
